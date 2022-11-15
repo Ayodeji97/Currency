6fc6f3917250ea09841d0d7e2e693493e2dbcb41
Ayodeji97 <danielayodeji97@gmail.com>
2022-11-16 00:37:57 +0100

    provide historical data repository interface

---
+++ app/src/main/java/com/danzucker/currency/di/repository/HistoricalDataRepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.danzucker.currency.di.repository
+
+import com.danzucker.currency.business.datasource.remote.remotesource.history.HistoricalDataRemoteSourceImpl
+import com.danzucker.currency.business.repository.history.HistoricalDataRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class HistoricalDataRepositoryModule {
+    @Binds
+    abstract fun provideHistoricalDataRepository(
+        historicalDataRemoteSourceImpl: HistoricalDataRemoteSourceImpl
+    ): HistoricalDataRepository
+}
