b4ae3d7f1e36f401200422fc216665a8d5f8647b
Ayodeji97 <danielayodeji97@gmail.com>
2022-11-16 07:51:36 +0100

    unit test currency symbol mapper

--- app/src/test/java/com/danzucker/currency/business/utils/DummyData.kt
+++ app/src/test/java/com/danzucker/currency/business/utils/DummyData.kt
@@ -3,6 +3,7 @@ package com.danzucker.currency.business.utils
 import com.danzucker.currency.business.datasource.remote.model.convert.ConvertCurrencyDto
 import com.danzucker.currency.business.datasource.remote.model.convert.InfoDto
 import com.danzucker.currency.business.datasource.remote.model.convert.QueryDto
+import com.danzucker.currency.business.datasource.remote.model.symbols.CurrencySymbolsDto
 
 object DummyData {
 
@@ -30,6 +31,28 @@ object DummyData {
         info = info,
         query = queryDto,
         result = 42.657,
+        success = true
+    )
+
+    val currencySymbolDto = CurrencySymbolsDto(
+        success = true,
+        symbols = hashMapOf("EUR" to "Europe", "AWZ" to "Aruban Florin")
+    )
+
+    val convertSymbolDto =  ConvertCurrencyDto(
+        date = "2022-12-09",
+        info = info,
+        query = queryDto,
+        result = 111.657,
+        success = true
+    )
+
+
+    val convertSymbolDto2 =  ConvertCurrencyDto(
+        date = "2022-02-09",
+        info = info,
+        query = queryDto,
+        result = 111.657,
         success = true
     )
 
