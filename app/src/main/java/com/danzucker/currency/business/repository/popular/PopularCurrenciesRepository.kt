5feb9791c27986f9ca7a08c10c0807a3fa12809e
Ayodeji97 <danielayodeji97@gmail.com>
2022-11-16 02:20:55 +0100

    implement popular currencies repo

--- app/src/main/java/com/danzucker/currency/business/repository/popular/PopularCurrenciesRepository.kt
+++ app/src/main/java/com/danzucker/currency/business/repository/popular/PopularCurrenciesRepository.kt
@@ -1,4 +1,13 @@
 package com.danzucker.currency.business.repository.popular
 
+import com.danzucker.currency.business.domain.model.popular.PopularCurrencies
+import com.danzucker.currency.business.utils.Result
+import kotlinx.coroutines.flow.Flow
+
 interface PopularCurrenciesRepository {
+
+    suspend fun getPopularCurrencies(
+        base: String,
+        symbols: String
+    ) : Flow<Result<PopularCurrencies>>
 }
--- app/src/main/java/com/danzucker/currency/business/repository/popular/PopularCurrenciesRepositoryImpl.kt
+++ app/src/main/java/com/danzucker/currency/business/repository/popular/PopularCurrenciesRepositoryImpl.kt
@@ -1,4 +1,33 @@
 package com.danzucker.currency.business.repository.popular
 
-class PopularCurrenciesRepositoryImpl {
+import com.danzucker.currency.business.datasource.remote.remotesource.popular.PopularCurrenciesRemoteSource
+import com.danzucker.currency.business.domain.model.popular.PopularCurrencies
+import com.danzucker.currency.business.utils.Result
+import com.danzucker.currency.business.utils.mapper.remotemapper.PopularCurrenciesDtoMapper
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class PopularCurrenciesRepositoryImpl @Inject constructor(
+    private val popularCurrenciesRemoteSource: PopularCurrenciesRemoteSource,
+    private val popularCurrenciesDtoMapper: PopularCurrenciesDtoMapper
+) : PopularCurrenciesRepository {
+    override suspend fun getPopularCurrencies(
+        base: String,
+        symbols: String
+    ): Flow<Result<PopularCurrencies>> =
+        flow {
+            when (val response =
+                popularCurrenciesRemoteSource.getPopularCurrencies(base, symbols)) {
+                is Result.Success -> {
+                    response.data?.let {
+                        val popularCurrencies = popularCurrenciesDtoMapper.transformToDomain(it)
+                        emit(Result.Success(popularCurrencies))
+                    }
+                }
+                is Result.Error -> {
+                    emit(Result.Error(response.errorMessage))
+                }
+            }
+        }
 }
